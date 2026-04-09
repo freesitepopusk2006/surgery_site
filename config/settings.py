f7a40688716bedb3f741b2ab1ec9b1f75dc51d0e
Pipusk <klimentfeofilov@gmail.com>
2026-04-09 23:31:45 +0300

    Add requirements.txt for Railway

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = [
+    ".up.railway.app",
+    "127.0.0.1",
+    "localhost",
+]
